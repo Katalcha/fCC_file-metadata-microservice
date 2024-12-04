4af049c5e88c88d856c06d4d5c06320e1767ed25
R.Evers <83616447+Katalcha@users.noreply.github.com>
2024-12-04 22:42:10 +0100

    feat: finish project
    add multer dependency
    
    use multer middleware to get info from fileupload

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const multer = require('multer');
 const express = require('express');
 const cors = require('cors');
 
@@ -9,6 +10,16 @@ app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', (_, res) => res.sendFile(process.cwd() + '/views/index.html'));
 
-
+app.post('/api/fileanalyse', multer().single('upfile'), (req, res) => {
+  try {
+    res.json({ 
+      name: req.file.originalname, 
+      type: req.file.mimetype, 
+      size: req.file.size 
+    });
+  } catch(err) {
+    res.json({ Error: err });
+  }
+});
 
 app.listen(process.env.PORT || 3000, () => console.log('Your app is listening on port ' + process.env.PORT));
