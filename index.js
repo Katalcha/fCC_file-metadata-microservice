9f2053f66abeb2ac0c29626c74f3a4e5564307b6
R.Evers <83616447+Katalcha@users.noreply.github.com>
2024-12-04 22:29:33 +0100

    refactor: swap var for const

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-var express = require('express');
-var cors = require('cors');
 require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
+app.get('/', (_, res) => res.sendFile(process.cwd() + '/views/index.html'));
 
 
 
-
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
-});
+app.listen(process.env.PORT || 3000, () => console.log('Your app is listening on port ' + process.env.PORT));
